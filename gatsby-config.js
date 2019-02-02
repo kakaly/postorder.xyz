d90181e07ddf4a628ec91f6b9904124f2b37aaac
kakaly <reach.kkarthik@gmail.com>
2019-02-02 14:36:51 -0800

    update analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-130227707-1`,
+        trackingId: `UA-133659889-1`,
       },
     },
     {
